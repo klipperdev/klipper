9e0774306f020d9bbb5a9a02a4960ecc0cf4da98
François Pluchino <francois.pluchino@klipper.dev>
2020-05-29 14:38:58 +0200

    Add metadata oauth scope loader

---
+++ src/Klipper/Bundle/ApiSecurityOauthBundle/Scope/Loader/MetadataScopeLoader.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Klipper\Bundle\ApiSecurityOauthBundle\Scope\Loader;
+
+use Klipper\Component\Metadata\MetadataManagerInterface;
+use Klipper\Component\SecurityOauth\Scope\Loader\ScopeLoaderInterface;
+
+/**
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+class MetadataScopeLoader implements ScopeLoaderInterface
+{
+    public const READ_ACTIONS = [
+        'list',
+        'view',
+    ];
+
+    public const MANAGE_ACTIONS = [
+        'create',
+        'upsert',
+        'update',
+        'delete',
+        'undelete',
+        'creates',
+        'upserts',
+        'updates',
+        'deletes',
+        'undeletes',
+    ];
+
+    private MetadataManagerInterface $metadataManager;
+
+    public function __construct(MetadataManagerInterface $metadataManager)
+    {
+        $this->metadataManager = $metadataManager;
+    }
+
+    public function load(): array
+    {
+        $validScopes = [];
+
+        foreach ($this->metadataManager->all() as $metadata) {
+            $read = false;
+            $manage = false;
+
+            if ($metadata->isPublic()) {
+                foreach ($metadata->getActions() as $action) {
+                    if (\in_array($action->getName(), static::READ_ACTIONS, true)) {
+                        $read = true;
+                        $validScopes[] = sprintf('meta/%s.readonly', $metadata->getName());
+                    } elseif (\in_array($action->getName(), static::MANAGE_ACTIONS, true)) {
+                        $manage = true;
+                        $validScopes[] = sprintf('meta/%s', $metadata->getName());
+                    }
+
+                    if ($read && $manage) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        sort($validScopes);
+
+        return $validScopes;
+    }
+}
