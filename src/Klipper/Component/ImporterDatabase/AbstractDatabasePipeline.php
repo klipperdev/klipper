1b8b76c6ef4f1ad41636e1de65f808877baba99f
François Pluchino <francois.pluchino@klipper.dev>
2020-07-08 12:12:41 +0200

    Make optional the batch size for the import pipeline

--- src/Klipper/Component/ImporterDatabase/AbstractDatabasePipeline.php
+++ src/Klipper/Component/ImporterDatabase/AbstractDatabasePipeline.php
@@ -17,23 +17,31 @@ use Doctrine\DBAL\DriverManager;
 use Doctrine\DBAL\Query\QueryBuilder;
 use Klipper\Component\Importer\Exception\InvalidArgumentException;
 use Klipper\Component\Importer\Pipeline\AbstractPipeline;
+use Klipper\Component\Importer\Pipeline\BatchablePipelineInterface;
 use Psr\Log\LoggerInterface;
 
 /**
  * @author François Pluchino <francois.pluchino@klipper.dev>
  */
-abstract class AbstractDatabasePipeline extends AbstractPipeline
+abstract class AbstractDatabasePipeline extends AbstractPipeline implements BatchablePipelineInterface
 {
     protected array $connectionParams = [];
+
+    protected int $batchSize;
 
     protected ?Connection $connection = null;
 
     /**
      * @param array|Connection $connectionParams The DBAL connection instance or config
      */
-    public function __construct($connectionParams, ?LoggerInterface $logger = null, int $batchSize = 1000)
-    {
-        parent::__construct($logger, $batchSize);
+    public function __construct(
+        $connectionParams,
+        ?LoggerInterface $logger = null,
+        int $batchSize = 1000
+    ) {
+        parent::__construct($logger);
+
+        $this->batchSize = $batchSize;
 
         if ($connectionParams instanceof Connection) {
             $this->connectionParams = [];
@@ -47,16 +55,23 @@ abstract class AbstractDatabasePipeline extends AbstractPipeline
         }
     }
 
+    public function getBatchSize(): int
+    {
+        return $this->batchSize;
+    }
+
     /**
      * @throws DBALException
      */
     public function extract(int $cursor, ?\DateTimeInterface $startAt = null): array
     {
         $connexion = $this->getConnexion();
+        $max = $this->getBatchSize() > 0 ? $this->getBatchSize() : null;
+        $first = null !== $max ? $cursor * $this->getBatchSize() : null;
 
         $res = $this->buildQueryBuilder($connexion, $startAt)
-            ->setMaxResults($this->getBatchSize())
-            ->setFirstResult($cursor * $this->getBatchSize())
+            ->setMaxResults($max)
+            ->setFirstResult($first)
             ->execute()
         ;
 
